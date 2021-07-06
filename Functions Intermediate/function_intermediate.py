070087bdfd1efe82cae65a7a29f17c38a2fb7013
Dylan-Barclay <dr.barclay99@gmail.com>
2021-07-06 16:13:53 -0500

    rough draft

--- Functions Intermediate/function_intermediate.py
+++ Functions Intermediate/function_intermediate.py
@@ -1,13 +1,91 @@
 x = [ [5,2,3], [10,8,9] ] 
 students = [
-     {'first_name':  'Michael', 'last_name' : 'Jordan'},
-     {'first_name' : 'John', 'last_name' : 'Rosales'}
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
 ]
 sports_directory = {
     'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
     'soccer' : ['Messi', 'Ronaldo', 'Rooney']
 }
 z = [ {'x': 10, 'y': 20} ]
-
-x[1[0]] = 15
+# Changing items in the lists
+x[1][0]= 15
 print(x)
+
+print(" ")
+
+students[0]['last_name'] = 'Bryant'
+print(students)
+
+print(" ")
+
+sports_directory['soccer'][0] = 'Andres'
+print(sports_directory)
+
+print(" ")
+
+z[0]['y'] = 30
+print(z)
+
+print(" ")
+
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+def iterateDictionary(students):
+    for student in students:
+        print(student)
+iterateDictionary(students)
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+
+print(" ")
+
+def iterateDictionary2(name, students):
+    for student in students:  
+        print(student[name])
+iterateDictionary2('first_name', students)
+# you can change 'first_name' to 'last_name' for last name...
+print(" ")
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+def printInfo(dojo):
+    print(str(len(dojo['locations'])) + ' LOCATIONS')
+    for loc in dojo['locations']:
+        print(loc)
+    print(str(len(dojo['instructors'])) + ' INSTRUCTORS')
+    for name in dojo['instructors']:
+        print(name)
+
+printInfo(dojo)
+
+# output:
+# 7 LOCATIONS
+# San Jose
+# Seattle
+# Dallas
+# Chicago
+# Tulsa
+# DC
+# Burbank
+    
+# 8 INSTRUCTORS
+# Michael
+# Amy
+# Eduardo
+# Josh
+# Graham
+# Patrick
+# Minh
+# Devon
